b7ff22b3663a55d527d79e27524df53a4550e435
David Hu <davidhu314@gmail.com>
2025-08-24 22:57:00 -0700

    fix

--- src/resources/v1/files/upload-urls/resource-client.ts
+++ src/resources/v1/files/upload-urls/resource-client.ts
@@ -9,64 +9,10 @@ import {
 import * as requests from "magic-hour/resources/v1/files/upload-urls/request-types";
 import { Schemas$V1FilesUploadUrlsCreateBody } from "magic-hour/types/v1-files-upload-urls-create-body";
 import { Schemas$V1FilesUploadUrlsCreateResponse } from "magic-hour/types/v1-files-upload-urls-create-response";
-import { FilesClient } from "magic-hour/resources/v1/files";
-import {
-  GenerateOptions,
-  GenerateRequestType,
-} from "magic-hour/helpers/generate-type";
-import { downloadFiles } from "magic-hour/helpers/download";
-import { ImageProjectsClient } from "magic-hour/resources/v1/image-projects";
-
-type GenerateRequest = GenerateRequestType<requests.CreateRequest, {}>;
 
 export class UploadUrlsClient extends CoreResourceClient {
   constructor(coreClient: CoreClient, opts: ResourceClientOptions) {
     super(coreClient, opts);
-  }
-
-  /**
-   * Generate asset upload urls
-   *
-   * Generates a list of pre-signed upload URLs for the assets required - Generate with automatic polling and downloading
-   *
-   * - This method provides a convenient way to create a request and automatically wait for completion and download outputs.
-   *
-   */
-  async generate(request: GenerateRequest, opts: GenerateOptions = {}) {
-    const {
-      waitForCompletion = true,
-      downloadOutputs = true,
-      downloadDirectory = undefined,
-      ...createOpts
-    } = opts;
-
-    const createResponse = await this.create(
-      {
-        ...request,
-      },
-      createOpts,
-    );
-
-    const projectsClient = new ImageProjectsClient(this._client, this._opts);
-
-    const result = await projectsClient.checkResult(
-      { id: createResponse.id },
-      {
-        waitForCompletion,
-        downloadOutputs,
-        downloadDirectory,
-        ...createOpts,
-      },
-    );
-
-    if (downloadOutputs) {
-      result.downloadedPaths = await downloadFiles(
-        result.downloads,
-        downloadDirectory,
-      );
-    }
-
-    return result;
   }
 
   /**
