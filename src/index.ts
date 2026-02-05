d8b0ac5f17a14cb94d139036f653de8cc8fe43b4
David Hu <davidhu314@gmail.com>
2026-02-05 10:49:16 -0800

    add-named-export

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export {
   isUploadFile,
 } from "make-api-request-js";
 
-export { Client as default } from "magic-hour/client";
+export { Client, Client as default } from "magic-hour/client";
 export { Environment } from "magic-hour/environment";
 export * as resources from "magic-hour/resources";
 export * as types from "magic-hour/types";
